04d1c6837d0fc01d4a5cfb0444c371e34d2e45fc
LucasBywalec <lukasz.bywalec@outlook.com>
2023-03-27 15:16:07 +0200

    changed the appearence of buttons

--- lab3/js/source/server.js
+++ lab3/js/source/server.js
@@ -18,11 +18,23 @@ app.get('/', (req, res) => {
     if(audioFile != undefined)
     {
       sentText += `<audio id=audioPlayer src="${audioFile}"> </audio>`
+      sentText += 
+      `<script>
+        function cancelVideo(){
+          let element = document.getElementById('videoPlayer');
+          element.src = "cancel.mp4";
+        }`
     }
 
     if(videoFile != undefined)
     {
       sentText += `<video id=videoPlayer src="${videoFile}"> </video>`
+      sentText += 
+      `function cancelAudio(){
+          let element = document.getElementById('audioPlayer');
+          element.src = "cancel.mp3";
+        }
+      </script>`
     }
 
     if(imgFile != undefined){
@@ -31,20 +43,6 @@ app.get('/', (req, res) => {
 
     sentText += `<button id="videoCancel" onClick="cancelVideo()">VIDEO</button>`
     sentText += `<button id="audioCancel" onClick="cancelAudio()">AUDIO</button>`
-
-
-    sentText += 
-    `<script>
-      function cancelVideo(){
-        let element = document.getElementById('videoPlayer');
-        element.src = "cancel.mp4";
-      }
-
-      function cancelAudio(){
-        let element = document.getElementById('audioPlayer');
-        element.src = "cancel.mp3";
-      }
-    </script>`
 
     res.send(sentText);
 })
