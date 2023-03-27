c0bffa31317a3dd1c03d73f75ed6b5afc3600eb4
LucasBywalec <lukasz.bywalec@outlook.com>
2023-03-27 15:10:00 +0200

    fixed errors - now src is changing

--- lab3/js/source/server.js
+++ lab3/js/source/server.js
@@ -10,19 +10,42 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    let sentText = "";
+    let sentText = ``;
     let audioFile = req.query.audioFile;
     let videoFile = req.query.videoFile;
+    let imgFile = req.query.imgFile;
 
     if(audioFile != undefined)
     {
-      sentText += `<audio id=audioPlayer src=` + audioFile + `> </audio>`
+      sentText += `<audio id=audioPlayer src="${audioFile}"> </audio>`
     }
 
     if(videoFile != undefined)
     {
-      sentText += `<video id=videoPlayer src=` + videoFile + `> </video>`
+      sentText += `<video id=videoPlayer src="${videoFile}"> </video>`
     }
+
+    if(imgFile != undefined){
+      sentText +=  `<img id="posterImage" src="${imgFile}"></img>`
+    }
+
+    sentText += `<button id="videoCancel" onClick="cancelVideo()">VIDEO</button>`
+    sentText += `<button id="audioCancel" onClick="cancelAudio()">AUDIO</button>`
+
+
+    sentText += 
+    `<script>
+      function cancelVideo(){
+        let element = document.getElementById('videoPlayer');
+        element.src = "cancel.mp4";
+      }
+
+      function cancelAudio(){
+        let element = document.getElementById('audioPlayer');
+        element.src = "cancel.mp3";
+      }
+    </script>`
+
     res.send(sentText);
 })
 
